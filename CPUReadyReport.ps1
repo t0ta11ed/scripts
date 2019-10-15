97bb59318f417cd4cc99b73690096fc7f21d849e
t0ta11ed <30156914+t0ta11ed@users.noreply.github.com>
2019-10-15 16:20:30 -0400

    Create CPUReadyReport.ps1

---
+++ CPUReadyReport.ps1
@@ -0,0 +1,45 @@
+Import-Module MySQLLib
+Import-Module VMware.VimAutomation.Core
+
+$conn = New-MySQLConnection -server x.x.x.x -database someDB -user someUser -password UserPw
+Invoke-MySQLQuery "DELETE FROM ClusterCPUReady" -connection $conn
+$start = get-date
+$over10 = New-Object System.Collections.ArrayList
+
+Connect-VIServer -Server somevCenter -User SomeUser -Password 'UserPw' | Out-Null
+$esxhosts = @('host1','host2','host3')
+
+foreach ($hostname in $esxhosts) { 
+
+$interval = 5
+$stat = 'cpu.ready.summation'
+$finish = Get-Date
+$start = ($finish).AddHours(- $interval)
+
+Get-Stat -Entity $entity -Stat $stat -Start $start -Finish $finish -Instance "" -MaxSamples 1 -Realtime | select -last 10 |
+
+Group-Object -Property {$_.Entity.Name} | %{
+
+    $_.Group |
+%{
+
+            $VM = $_.Entity.Name
+            $timeStamp = $_.Timestamp
+            $readyMs = $_.Value
+            $readyPerc = "{0:P2}" -f ($_.Value/($_.Intervalsecs*1000))
+            $esxi = $_.Entity.VMHost
+            Invoke-MySQLQuery "INSERT into ClusterCPUReady (Date,VM,ReadyMs,ReadyPerc,EsxHost,Location) VALUES ('$timeStamp','$VM','$readyMs','$readyPerc','$esxi','SomeSite')" -conn $conn
+            if ($readyMs -ge "2000") { $over10.Add("$timestamp $VM $readyMs $readyPerc $esxi") | Out-Null }
+        } 
+
+    } 
+}
+ Disconnect-VIServer * -Confirm:$false 
+ 
+$end = get-date
+$elapsedTime = New-Timespan -Start $start -End $end
+
+if ($over10) {
+    # Send and email.
+    Send-MailMessage
+    }
